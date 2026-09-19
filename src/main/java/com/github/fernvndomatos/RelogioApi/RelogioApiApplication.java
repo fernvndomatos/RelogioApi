63fdca9fc8800823d006c97bd66dc912b2f701d9
Fernando Matos <fernando.matos221@gmail.com>
2026-09-19 12:22:28 -0300

    Initial commit: estrutura base do projeto Spring Boot

---
+++ src/main/java/com/github/fernvndomatos/RelogioApi/RelogioApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.fernvndomatos.RelogioApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelogioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelogioApiApplication.class, args);
+	}
+
+}
